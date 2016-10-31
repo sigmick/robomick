dae2b548bc082e93cf9a4c7e7f21c52dc27bdb62
Sigkawat <sigmick@hotmail.com>
2016-11-01 03:29:04 +0700

    no message

--- bot.php
+++ bot.php
@@ -250,7 +250,7 @@ function getUser($userid){
 function get_tel($search) {
     $search=str_replace(" ","+",$search);
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, 'http://searchapi.yellowpages.co.th/api.jsp?id=&txtWhat='.$search.'&language=th&hits=5&page=1');
+	curl_setopt($ch, CURLOPT_URL, 'http://searchapi.yellowpages.co.th/api.jsp?id=&txtWhat='.$search.'&language=th&hits=1&page=1');
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
