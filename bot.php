ac3fbc71782c8c081812df3420140f5db796a713
Sigkawat <sigmick@hotmail.com>
2016-11-01 03:30:05 +0700

    no message

--- bot.php
+++ bot.php
@@ -249,6 +249,7 @@ function getUser($userid){
 
 function get_tel($search) {
     $search=str_replace(" ","+",$search);
+    $search=urlencode($search);
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, 'http://searchapi.yellowpages.co.th/api.jsp?id=&txtWhat='.$search.'&language=th&hits=1&page=1');
 	curl_setopt($ch, CURLOPT_POST, 1);
