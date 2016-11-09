03ccff83e79fea92d11fa54cb16673e90f087c4b
Sigkawat <MickiMac@Sigkawats-iMac.local>
2016-11-09 23:20:59 +0700

    test

--- bot.php
+++ bot.php
@@ -226,8 +226,10 @@ if (!is_null($events['events'])) {
 
                 //return json_decode($result, true);
                 echo "OK";
-                die();
-            }
+                
+            }else{
+
+            
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -249,7 +251,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 
-            
+            }
 		}
 	}
 }
