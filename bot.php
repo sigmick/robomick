59fb5f19b345e2aea0a285e7fbcab4d71b7e5103
Sigkawat <MickiMac@Sigkawats-iMac.local>
2016-11-10 00:36:00 +0700

    test

--- bot.php
+++ bot.php
@@ -215,26 +215,26 @@ if (!is_null($events['events'])) {
 
                $curl = curl_init();
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://api.line.me/v2/bot/group/C57c1cb06e2b7de79955ee091e16050c3/leave",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer w/CaCXolKorjagsQzBTgKYovOd4fiJrS9ez0Qh8rY0S8YVjIOnJBT1P1JmVXI5Bh+XAdN2sk521x7GaYlnAQi3+QUCaDmgzx+rlX5wRubhF1BtwOiiOsB4NyfwJ/FMyKsHoy6sB4E5wa059pme9rKwdB04t89/1O/w1cDnyilFU=",
-    "cache-control: no-cache",
-    "content-type: application/json",
-    "postman-token: 43678754-1a2d-f371-2267-3a8a54345564"
-  ),
-));
+                curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.line.me/v2/bot/group/C57c1cb06e2b7de79955ee091e16050c3/leave",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_HTTPHEADER => array(
+                    "authorization: Bearer w/CaCXolKorjagsQzBTgKYovOd4fiJrS9ez0Qh8rY0S8YVjIOnJBT1P1JmVXI5Bh+XAdN2sk521x7GaYlnAQi3+QUCaDmgzx+rlX5wRubhF1BtwOiiOsB4NyfwJ/FMyKsHoy6sB4E5wa059pme9rKwdB04t89/1O/w1cDnyilFU=",
+                    "cache-control: no-cache",
+                    "content-type: application/json",
+                    "postman-token: 43678754-1a2d-f371-2267-3a8a54345564"
+                ),
+                ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+                curl_close($curl);
                 $messages = [
                     'type' => 'text',
                     'text' => $httpcode 
